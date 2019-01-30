d8fb25dbfaf18ff52a6e1db0b98f179287c4dc19
visualstudio <visualstudio@122-PC21>
2019-01-30 12:22:35 -0500

    this change

--- C0722852_WEEK_01/Program.cs
+++ C0722852_WEEK_01/Program.cs
@@ -10,6 +10,14 @@ namespace C0722852_WEEK_01
     {
         static void Main(string[] args)
         {
+            int a, b, c, sum;
+            Console.Write("Enter three whole numbers:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            sum = a + b + c;
+            Console.Write("The sum is: ={0}", sum);
+            Console.ReadKey();
         }
     }
 }
